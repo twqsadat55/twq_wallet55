c67f6aaeca0ad63bb182893b6e402696b1dc1a3c
twqsadat55 <twq.sadat@gmail.com>
2025-04-29 13:47:24 +0430

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
 // service worker
+const cacheName = "expense-tracker-v1";
+const assets = [
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./chart.min.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
